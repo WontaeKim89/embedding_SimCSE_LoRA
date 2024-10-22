a3514b4e30e70499652a2e39178afbfd999c75ca
wtkim <wtkim@douzone.com>
2024-10-22 17:52:50 +0900

    feat. Lora 주석처리된 클래스 부분 제거

--- model.py
+++ model.py
@@ -4,45 +4,6 @@ from transformers import RobertaModel, RobertaTokenizer
 from transformers.models.roberta.modeling_roberta import RobertaSelfAttention
 
 
-# class LoRA(nn.Module):
-#     def __init__(self, model, rank=8, dropout_rate=0.1):
-#         super(LoRA, self).__init__()
-#         self.model = model
-#         self.rank = rank
-
-#         # LoRA 적용을 위한 저차원 행렬 정의
-#         self.lora_q = nn.Linear(model.config.hidden_size, rank, bias=False)
-#         self.lora_k = nn.Linear(rank, model.config.hidden_size, bias=False)
-
-#         # 드롭아웃 레이어 추가
-#         self.dropout = nn.Dropout(p=dropout_rate)
-
-#         # 저차원 행렬 가중치 초기화
-#         nn.init.normal_(self.lora_q.weight, std=model.config.initializer_range)
-#         nn.init.zeros_(self.lora_k.weight)
-
-#     def forward(self, input_ids, attention_mask, **kwargs):
-        
-#         # 모델에 입력할 데이터의 차원을 [batch_size, sequence_length]로 맞출것
-#         if input_ids.ndimension() == 3:
-#             input_ids = input_ids.squeeze(0)
-#         if input_ids.ndimension() == 3:
-#             attention_mask = attention_mask.squeeze(0)
-#         outputs = self.model(input_ids=input_ids, attention_mask=attention_mask)
-#         hidden_states = outputs.last_hidden_state
-#         # LoRA 적용: 저차원 행렬을 사용하여 attention 수정
-#         lora_q_output = self.lora_q(hidden_states)
-#         lora_k_output = self.lora_k(lora_q_output)
-#         # 드롭아웃 적용
-#         lora_k_output = self.dropout(lora_k_output)
-
-#         # LoRA 결과를 원본 히든 상태에 추가
-#         adapted_hidden_states = hidden_states + lora_k_output
-#         return adapted_hidden_states
-
-
-
-
 class LoRA(nn.Module):
     def __init__(self, model, r=8, alpha=16, dropout_rate=0.1):
         super(LoRA, self).__init__()
